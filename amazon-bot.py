ad44e17422edcab17b796765632aad2a5e3bc664
Karan <brarkaran938@gmail.com>
2022-02-06 17:44:42 -0600

    Added code to send mail using the smtp library

--- amazon-bot.py
+++ amazon-bot.py
@@ -1,9 +1,13 @@
 # 1) Check price of product
 # 2) Get notified via email when price goes below a certain point
+# 3) Keep checking price and send email automatically after a given period of seconds once the script has been run, 
 
-from itertools import product
+from email import message
 import requests
 from lxml import html
+import smtplib, ssl
+import getpass
+import time
 
 def get_price():
 
@@ -18,10 +22,36 @@ def get_price():
     price = tree.xpath('//span[@class="a-offscreen"]/text()')[0]
     return price.replace("$","")
 
+def send_email(price, password):
+
+    smtp_server = "smtp.gmail.com"
+    port = 587 
+    sender_email = "senseicode777@gmail.com"
+
+    context = ssl.create_default_context()
+
+    try:
+        server = smtplib.SMTP(smtp_server,port)
+        server.ehlo()
+        server.starttls(context=context)
+        server.ehlo()
+        server.login(sender_email, password)
+        message = f'Your product is now {price}'
+        server.sendmail(sender_email, sender_email, message)
+        print("Email sent!")
+    except Exception as e:
+        print("Error sending email")
+        print(e)
+        quit()
+    finally:
+        server.quit() 
 
 if __name__ == "__main__":
 
-    price = get_price()
-
-    if float(price) < float(250):
-        print('Send email')
+    password = getpass.getpass()
+    while(True):
+        price = get_price()
+        if float(price) < float(270):
+            send_email(price, password)
+        time.sleep(86400)
+        
